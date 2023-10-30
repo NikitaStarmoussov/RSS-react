9fec6e5769e5eee27fb0a2177eeb35dc189eda52
NikitaStarmoussov <nikta.starmousov@gmail.com>
2023-10-30 15:28:32 +0500

    feat: add basic get items api function

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,13 @@ import { useState } from "react";
 // import viteLogo from "/vite.svg";
 import "./App.css";
 
+async function getItems(){
+  const response = await fetch('https://swapi.dev/api/films');
+  const data = await response.json();
+  console.log(data);
+}
 function App() {
+  getItems();
   const [count, setCount] = useState(0);
 
   return (
