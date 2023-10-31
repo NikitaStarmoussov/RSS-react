97696b0c2e43e7b053b336f80df510e2893165da
NikitaStarmoussov <nikta.starmousov@gmail.com>
2023-10-31 16:43:53 +0500

    feat: add film list

---
+++ src/components/FilmList.tsx
@@ -0,0 +1,46 @@
+import { ChangeEvent } from "react";
+import FilmListItem from "./FilmListItem";
+import SearchForm from "./SearchForm";
+import Film from "../types/types";
+
+interface FilmListProps {
+  films: Film[];
+  searchQuery: string;
+  isLoading: boolean;
+  onSearchChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  onSearchSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
+}
+
+const FilmList: React.FC<FilmListProps> = ({
+  films,
+  searchQuery,
+  isLoading,
+  onSearchChange,
+  onSearchSubmit,
+}) => {
+  const filteredFilms = films.filter((film) =>
+    film.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  return (
+    <div>
+      <SearchForm
+        searchQuery={searchQuery}
+        isLoading={isLoading}
+        onSearchChange={onSearchChange}
+        onSearchSubmit={onSearchSubmit}
+      />
+      {filteredFilms.length > 0 ? (
+        <ul>
+          {filteredFilms.map((film) => (
+            <FilmListItem key={film.url} film={film} />
+          ))}
+        </ul>
+      ) : (
+        <p>Нет результатов поиска</p>
+      )}
+    </div>
+  );
+};
+
+export default FilmList;
