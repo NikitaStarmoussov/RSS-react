d6420376246e0b2bce9a00ec3da4b1603b6e6d80
NikitaStarmoussov <nikta.starmousov@gmail.com>
2023-10-31 16:45:39 +0500

    featL update app and main tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,67 @@
-import React from "react";
-import "./App.css";
-import { CardList } from "./components/card-list";
+import React, { Component, ChangeEvent } from 'react';
+import Film from './types/types';
+import FilmList from './components/FilmList';
 
+interface AppProps {}
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <CardList />
-      </div>   
-    );
+interface AppState {
+  films: Film[];
+  searchQuery: string;
+  isLoading: boolean;
+}
+
+class App extends Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
+    this.state = {
+      films: [],
+      searchQuery: '',
+      isLoading: true,
+    };
+  }
+ 
+  componentDidMount() {
+    this.fetchFilms();
   }
 
-  
+  fetchFilms = () => {
+    this.setState({ isLoading: true });
+
+    fetch('https://swapi.dev/api/films')
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ films: data.results, isLoading: false });
+      })
+      .catch((error) => {
+        console.error('Ошибка при получении фильмов:', error);
+        this.setState({ isLoading: false });
+      });
+  };
+
+  handleSearchChange = (event: ChangeEvent<HTMLInputElement>) => {
+    this.setState({ searchQuery: event.target.value });
+  };
+
+  handleSearchSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    this.fetchFilms();
+  };
+
+  render() {
+    const { films, searchQuery, isLoading } = this.state;
+
+    return (
+      <div>
+        <FilmList
+          films={films}
+          searchQuery={searchQuery}
+          isLoading={isLoading}
+          onSearchChange={this.handleSearchChange}
+          onSearchSubmit={this.handleSearchSubmit}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
