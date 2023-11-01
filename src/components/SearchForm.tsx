d2d2861880d7d98f9a00b85dd280f9a3c33ddc9f
NikitaStarmoussov <nikta.starmousov@gmail.com>
2023-11-01 20:02:28 +0500

    feat: delete hook

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,43 +1,43 @@
-import React, { ChangeEvent, useEffect } from 'react';
+import React, { ChangeEvent } from 'react';
 
 interface SearchFormProps {
-    searchQuery: string,
-    isLoading: boolean, 
-    onSearchChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  searchQuery: string;
+  isLoading: boolean;
+  onSearchChange: (event: ChangeEvent<HTMLInputElement>) => void;
 }
 
-const SearchForm: React.FC<SearchFormProps> = ({isLoading}) => {
-  const [searchQuery, setSearchQuery] = React.useState('');
-
-  useEffect(() => {
-    const savedSearchQuery = localStorage.getItem('searchQuery');
-    if (savedSearchQuery) {
-      setSearchQuery(savedSearchQuery);
-    }
-  }, []);
-
-  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchQuery(event.target.value);
-    if (searchQuery.trim() === '') {
-        localStorage.removeItem('searchQuery');
-      } else {
-        localStorage.setItem('searchQuery', searchQuery);
-      }
+class SearchForm extends React.Component<SearchFormProps> {
+  state = {
+    searchQuery: localStorage.getItem('searchQuery') || '',
   };
 
+  handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const searchQuery = event.target.value;
+    this.setState({ searchQuery });
 
+    if (searchQuery.trim() === '') {
+      localStorage.removeItem('searchQuery');
+    } else {
+      localStorage.setItem('searchQuery', searchQuery);
+    }
+  };
 
-  return (
-    <form >
-      <input
-        type="text"
-        value={searchQuery}
-        onChange={handleSearchChange}
-        placeholder="Поиск фильмов..."
-      />
-      <button type="button" >{isLoading ? 'Загрузка' : 'Поиск'}</button>
-    </form>
-  );
-};
+  render() {
+    const { searchQuery } = this.state;
+    const { isLoading } = this.props;
+
+    return (
+      <form>
+        <input
+          type="text"
+          value={searchQuery}
+          onChange={this.handleSearchChange}
+          placeholder="Поиск фильмов..."
+        />
+        <button type="button">{isLoading ? 'Загрузка' : 'Поиск'}</button>
+      </form>
+    );
+  }
+}
 
 export default SearchForm;
