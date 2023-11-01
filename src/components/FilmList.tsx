b412a855a315e96a5ce6ad47bfc5a579356d2452
NikitaStarmoussov <nikta.starmousov@gmail.com>
2023-11-01 20:05:54 +0500

    feat: function=> class

--- src/components/FilmList.tsx
+++ src/components/FilmList.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent } from "react";
+import React, { ChangeEvent } from "react";
 import FilmListItem from "./FilmListItem";
 import SearchForm from "./SearchForm";
 import Film from "../types/types";
@@ -10,36 +10,59 @@ interface FilmListProps {
   onSearchChange: (event: ChangeEvent<HTMLInputElement>) => void;
 }
 
-const FilmList: React.FC<FilmListProps> = ({
-  films,
-  searchQuery,
-  isLoading,
-  onSearchChange,
-  
-}) => {
-  const filteredFilms = films.filter((film) =>
-    film.title.toLowerCase().includes(searchQuery.toLowerCase())
-  );
+interface FilmListState {
+  filteredFilms: Film[];
+}
 
-  return (
-    <div>
-      <SearchForm
-  searchQuery={searchQuery}
-  isLoading={isLoading}
-  onSearchChange={onSearchChange}
-  
-/>
-      {filteredFilms.length > 0 ? (
-        <ul>
-          {filteredFilms.map((film) => (
-            <FilmListItem key={film.url} film={film} />
-          ))}
-        </ul>
-      ) : (
-        <p>Нет результатов поиска</p>
-      )}
-    </div>
-  );
-};
+class FilmList extends React.Component<FilmListProps, FilmListState> {
+  constructor(props: FilmListProps) {
+    super(props);
+    this.state = {
+      filteredFilms: [],
+    };
+  }
+
+  componentDidMount() {
+    this.filterFilms();
+  }
+
+  componentDidUpdate(prevProps: FilmListProps) {
+    if (prevProps.films !== this.props.films || prevProps.searchQuery !== this.props.searchQuery) {
+      this.filterFilms();
+    }
+  }
+
+  filterFilms() {
+    const { films, searchQuery } = this.props;
+    const filteredFilms = films.filter((film) =>
+      film.title.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    this.setState({ filteredFilms });
+  }
+
+  render() {
+    const { searchQuery, isLoading, onSearchChange } = this.props;
+    const { filteredFilms } = this.state;
+
+    return (
+      <div>
+        <SearchForm
+          searchQuery={searchQuery}
+          isLoading={isLoading}
+          onSearchChange={onSearchChange}
+        />
+        {filteredFilms.length > 0 ? (
+          <ul>
+            {filteredFilms.map((film) => (
+              <FilmListItem key={film.url} film={film} />
+            ))}
+          </ul>
+        ) : (
+          <p>Нет результатов поиска</p>
+        )}
+      </div>
+    );
+  }
+}
 
 export default FilmList;
--- src/components/FilmListItem.tsx
+++ src/components/FilmListItem.tsx
@@ -1,16 +1,21 @@
 import Film from "../types/types";
+import React from 'react';
 
 interface FilmListItemProps {
     film: Film;
   }
   
-  const FilmListItem: React.FC<FilmListItemProps> = ({ film }) => {
-    return (
-      <li key={film.url}>
-        <h3>{film.title}</h3>
-        <p>{film.opening_crawl}</p>
-      </li>
-    );
-  };
+  class FilmListItem extends React.Component<FilmListItemProps> {
+    render() {
+      const { film } = this.props;
+  
+      return (
+        <li key={film.url}>
+          <h3>{film.title}</h3>
+          <p>{film.opening_crawl}</p>
+        </li>
+      );
+    }
+  }
   
   export default FilmListItem;
