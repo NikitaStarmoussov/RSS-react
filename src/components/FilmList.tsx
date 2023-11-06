3645be344b27ade36590d944f3814ae0439a540b
NikitaStarmoussov <nikta.starmousov@gmail.com>
2023-11-07 04:19:59 +0500

    feat: migrate to new api

--- src/components/FilmList.tsx
+++ src/components/FilmList.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import FilmListItem from "./FilmListItem";
 import SearchForm from "./SearchForm";
 import Item from "../types/types";
@@ -7,53 +7,50 @@ import { Link, useLoaderData, useNavigate} from "react-router-dom";
 export async function itemsLoader({ request }: { request: Request }) {
   let page = Number(new URL(request.url).searchParams.get('page'));
   let limit = Number(new URL(request.url).searchParams.get('limit'));
+  let query = new URL(request.url).searchParams.get('q');
   if(limit === 0){
     limit = 10
   }
   if(page === 0){
     page = 1
   }
+  if(!query){
+    query = ''
+  }
   
 
   const newOffset = (page - 1) * 10;
     
-      const response = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${newOffset}&limit=${limit}`);
+      const response = await fetch(`https://dummyjson.com/products/search?q=${query}&skip=${newOffset}&limit=${limit}`);
       const data = response.json();
       return data;
 
 }
 
 type Response = {
-  count: number;
-  next: string | null;
-  previous: string | null;
-  results: Item[]
+
+products: Item[]
 }
 
-    export const itemsWithPageLoader = async (search: string) => {
+    // export const itemsWithPageLoader = async (search: string) => {
       
 
-      const page = Number(new URLSearchParams(search).get('page'));
-      const newOffset = (page - 1) * 10;
-      const newLimit = page * 10;
+    //   const page = Number(new URLSearchParams(search).get('page'));
+    //   const newOffset = (page - 1) * 10;
+    //   const newLimit = page * 10;
     
-      const response = await fetch(`https://pokeapi.co/api/v2/ability/?offset=${newOffset}&limit=${newLimit}`);
-      const data = await response.json();
+    //   const response = await fetch(`https://pokeapi.co/api/v2/ability/? skip=${newOffset}&limit=${newLimit}`);
+    //   const data = await response.json();
     
-      return data;
-    };
+    //   return data;
+    // };
     
 
-export const fetchFilms = () => {
-};
 
-function getSearchQuery(newSearch?: string, addPage?:boolean, subtractPage?:boolean, newLimit?:number) {
+function getSearchQuery(addPage?:boolean, subtractPage?:boolean, newLimit?:number, query?:string) {
   const url = window.location.search
   const params = new URLSearchParams(url)
   const page = params.get('page')
-  if(newSearch){
-    params.set('search', newSearch)
-  }
   if(addPage){
     params.set('page', (Number(page) + 1).toString())
   }
@@ -62,6 +59,11 @@ function getSearchQuery(newSearch?: string, addPage?:boolean, subtractPage?:bool
   }
   if(newLimit){
     params.set('limit', newLimit.toString())
+    params.set('page', '1')
+  }
+  if(query){
+    params.set('q', query)
+    params.set('page', '1')
   }
   return params.toString()
   
@@ -69,7 +71,7 @@ function getSearchQuery(newSearch?: string, addPage?:boolean, subtractPage?:bool
 function FilmList() {
   const data = useLoaderData() 
 
-  const items = (data as Response).results
+  const items = (data as Response).products
 
   const [itemLimit, setItemLimit] = useState<number>(10);
 
@@ -80,20 +82,17 @@ function FilmList() {
   const searchSubmit = (query: string) => {
     navigate({
       pathname: '/',
-      search: getSearchQuery(query),
-    });
+      search: getSearchQuery(undefined, undefined, undefined, query),
+    })
   };
   const changeLimit = (limit: number) => {
     setItemLimit(limit)
     navigate({
       pathname: '/',
-      search: getSearchQuery(undefined, undefined, undefined, limit),
+      search: getSearchQuery( undefined, undefined, limit),
     });
   }
 
-  useEffect(() => {
-    fetchFilms();
-  }, []);
 
   return (
     <div>
@@ -106,8 +105,8 @@ function FilmList() {
   <option value="20">20</option>
 </select></div>
       <div style={{ display: "flex" }}>
-      <Link to={{ pathname: '/', search: getSearchQuery(undefined, undefined, true)}}>Prev</Link>
-      <Link to={{ pathname: '/', search: getSearchQuery(undefined, true)}}>Next</Link>
+      <Link to={{ pathname: '/', search: getSearchQuery(undefined, true)}}>Prev</Link>
+      <Link to={{ pathname: '/', search: getSearchQuery(true)}}>Next</Link>
       
       </div>
       {false ? (
@@ -115,7 +114,7 @@ function FilmList() {
 ) : items.length > 0 ? (
   <ul key={items.length}>
     {items.map((item) => (
-      <FilmListItem key={item.url} item={item} />
+      <FilmListItem key={item.id} item={item} />
     ))}
   </ul>
 ) : (
