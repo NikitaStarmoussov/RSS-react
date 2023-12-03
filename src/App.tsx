481068109536f4455a925b01af6c719140dbb08f
NikitaStarmoussov <nikta.starmousov@gmail.com>
2023-12-03 20:48:58 +0500

    feat: add uncontrolled form for saving in redux store

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,22 @@ import {
   BrowserRouter,
   Outlet
 } from "react-router-dom";
-import { Provider, useSelector } from 'react-redux';
+import { Provider, useDispatch, useSelector } from 'react-redux';
 import store from "./store";
+import { FormEvent } from "react";
+import { ThunkDispatch, AnyAction } from "@reduxjs/toolkit";
+import { actions } from "./slice";
 
 
+
+export interface User {
+  name: string;
+  age: number;
+  email: string;
+  password: string;
+  gender: string;
+  country: string;
+} 
 export default function App() {
   return (
     <Provider store={store}>
@@ -45,17 +57,11 @@ function Layout() {
       <Outlet/>
       </div>)}
 
-      interface User {
-        name: string;
-        age: number;
-        email: string;
-        password: string;
-        gender: string;
-        country: string;
-      } 
+     
 
 function Home() {
-  const items  = useSelector<ReturnType<typeof store.getState>>((state) => state.search);
+  const items  = useSelector<ReturnType<typeof store.getState>>((state) => state.search.data);
+  console.log(items);
   return <div>
     <h1>User</h1>
     <h2>Name: {(items as User).name}</h2>
@@ -66,9 +72,106 @@ function Home() {
     <h2>Country: {(items as User).country}</h2>
   </div>
 }
-
+interface CustomElements extends HTMLFormControlsCollection   {
+  name: HTMLInputElement;
+  age: HTMLInputElement;
+  email: HTMLInputElement;
+  password: HTMLInputElement;
+  confirmPassword: HTMLInputElement;
+  gender: HTMLInputElement;
+  termsAccepted: HTMLInputElement;
+  picture: HTMLInputElement;
+  country: HTMLInputElement;
+}
+ 
+interface CustomForm extends HTMLFormElement {
+  readonly elements: CustomElements;
+}
+export type AppDispatch = ThunkDispatch<User, unknown, AnyAction>;
 function Uncontrolled() {
-  return <h2>About</h2>;
+  const dispatch: AppDispatch = useDispatch();
+
+
+  const handleSubmit = (event : FormEvent<CustomForm>) => {
+    event.preventDefault();
+    const target = event.currentTarget.elements;
+    const data = {
+      name: target.name.value,
+      age: target.age.value,
+      email: target.email.value,
+      password: target.password.value,
+      confirmPassword: target.confirmPassword.value,
+      gender: target.gender.value,
+      termsAccepted: target.termsAccepted.checked,
+      picture: target.picture.value,
+      country: target.country.value
+    };
+    // Get form values
+
+    dispatch(actions.changeData(data));
+    // Do something with the form values
+    console.log(data)
+
+   
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">Name:</label>
+      <input type="text" id="name" name="name" required />
+      <br />
+
+      <label htmlFor="age">Age:</label>
+      <input type="number" id="age" name="age" min="0" required />
+      <br />
+
+      <label htmlFor="email">Email:</label>
+      <input type="email" id="email" name="email" required />
+      <br />
+
+      <label htmlFor="password">Password:</label>
+      <input type="password" id="password" name="password" required />
+      <br />
+
+      <label htmlFor="confirmPassword">Confirm Password:</label>
+      <input type="password" id="confirmPassword" name="confirmPassword" required />
+      <br />
+
+      <label>Gender:</label>
+      <label htmlFor="male">Male</label>
+      <input type="radio" id="male" name="gender" value="male" required />
+      <label htmlFor="female">Female</label>
+      <input type="radio" id="female" name="gender" value="female" required />
+      <br />
+
+      <label>
+        <input type="checkbox" name="termsAccepted" required />
+        Accept Terms & Conditions
+      </label>
+      <br />
+
+      <label htmlFor="picture">Upload Picture:</label>
+      <input type="file" id="picture" name="picture" accept="image/png, image/jpeg" />
+      <br />
+
+      <label htmlFor="country">Country:</label>
+      <select id="country" name="country" required>
+        <option value="">Select Country</option>
+        <option value="USA">USA</option>
+        <option value="India">India</option>
+        <option value="Australia">Australia</option>
+        <option value="UK">UK</option>
+        {/* {countries.map((country) => (
+          <option key={country.code} value={country.name}>
+            {country.name}
+          </option>
+        ))} */}
+      </select>
+      <br />
+
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
 
 function Similar() {
--- src/slice.ts
+++ src/slice.ts
@@ -1,38 +1,22 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 
-const initialState = {
+const initialState = {data:{
     name: "",
     age: 0,
     email:"",
     password: "",
     gender:"",
-    country: ""
+    country: ""}
 };
 
 const userSlice = createSlice({
   name: 'User',
   initialState,
   reducers: {
-     changeName: (state, action) => {
-      state.name = action.payload;
-    },
-    changeAge: (state, action) => {
-      state.age = action.payload;
-    },
-    changeEmail: (state, action) => {
-      state.email = action.payload;
-    },
-    changePassword: (state, action) => {
-      state.password = action.payload;
-    },
-    changeGender: (state, action) => {
-      state.gender = action.payload;
-    },
-    changeCountry: (state, action) => {
-      state.country = action.payload;
-    }
-}   
+    changeData: (state, action) => {
+        state.data = action.payload;
+}   }
 });
 
 export const { actions, reducer } = userSlice;
