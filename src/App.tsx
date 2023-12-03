12728bed5b4f7968fac44e5c4ab9edb76623838e
NikitaStarmoussov <nikta.starmousov@gmail.com>
2023-12-03 19:25:42 +0500

    feat: add basic router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,57 @@
 
-import './App.css'
-import Form from './components/form'
+import {
+  Routes ,Route,
+  Link,
+  BrowserRouter,
+  Outlet
+} from "react-router-dom";
 
-function App() {
-
-
+export default function App() {
   return (
-    <>
-     <Form />
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="users" element={<Users />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+function Layout() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/users">Users</Link>
+          </li>
+        </ul>
+      </nav>
+      <Outlet/>
+      </div>)}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
+function NoPage() {
+  return <h2>404 not found</h2>;
+}
